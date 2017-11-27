f5f433b2249c36bd65000cdb8fd6a6638ae4e415
alex3347 <821882886@qq.com>
2017-11-27 22:07:53 +0800

    buy page completed

--- src/pages/Buy/Buy.js
+++ src/pages/Buy/Buy.js
@@ -23,8 +23,59 @@ export default class Buy extends Component {
                             <img className={styles.itemLeft} src={require('./images/item1.png')} alt=""/>
                             <div className={styles.itemRight}>
                                 <div className={styles.content}>
-                                    <div className={styles.detail}>黄油 糖粉 全蛋液 低筋面粉 蜂蜜</div>
-                                    <div className={styles.price}>来自<i>莎拉布莱曼</i></div>
+                                    <div className={styles.detail}>舒克曼  威士宝黄油200g</div>
+                                    <div className={styles.price}>￥16.8<s>20.5</s></div>
+                                </div>
+                                <i className='iconfont icon-gouwuche'/>
+                            </div>
+                        </div>
+                        <div className={styles.similar}>
+                            <span>其他类似商品</span>
+                            <i className='iconfont icon-arrow-bottom'/>
+                        </div>
+                    </div>
+                    <div className={styles.item}>
+                        <div className={styles.itemTitle}>黄油/300g</div>
+                        <div className={styles.itemDetail}>
+                            <img className={styles.itemLeft} src={require('./images/item1.png')} alt=""/>
+                            <div className={styles.itemRight}>
+                                <div className={styles.content}>
+                                    <div className={styles.detail}>舒克曼  威士宝黄油200g</div>
+                                    <div className={styles.price}>￥16.8<s>20.5</s></div>
+                                </div>
+                                <i className='iconfont icon-gouwuche'/>
+                            </div>
+                        </div>
+                        <div className={styles.similar}>
+                            <span>其他类似商品</span>
+                            <i className='iconfont icon-arrow-bottom'/>
+                        </div>
+                    </div>
+                    <div className={styles.item}>
+                        <div className={styles.itemTitle}>黄油/300g</div>
+                        <div className={styles.itemDetail}>
+                            <img className={styles.itemLeft} src={require('./images/item1.png')} alt=""/>
+                            <div className={styles.itemRight}>
+                                <div className={styles.content}>
+                                    <div className={styles.detail}>舒克曼  威士宝黄油200g</div>
+                                    <div className={styles.price}>￥16.8<s>20.5</s></div>
+                                </div>
+                                <i className='iconfont icon-gouwuche'/>
+                            </div>
+                        </div>
+                        <div className={styles.similar}>
+                            <span>其他类似商品</span>
+                            <i className='iconfont icon-arrow-bottom'/>
+                        </div>
+                    </div>
+                    <div className={styles.item}>
+                        <div className={styles.itemTitle}>黄油/300g</div>
+                        <div className={styles.itemDetail}>
+                            <img className={styles.itemLeft} src={require('./images/item1.png')} alt=""/>
+                            <div className={styles.itemRight}>
+                                <div className={styles.content}>
+                                    <div className={styles.detail}>舒克曼  威士宝黄油200g</div>
+                                    <div className={styles.price}>￥16.8<s>20.5</s></div>
                                 </div>
                                 <i className='iconfont icon-gouwuche'/>
                             </div>
@@ -35,6 +86,7 @@ export default class Buy extends Component {
                         </div>
                     </div>
                 </div>
+                <div className={styles.block}/>
                 <div className={styles.btnContainer}>
                     <div className={styles.btn}>
                         结算
