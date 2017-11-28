adcbe7c74c57c17977e46c771b0386731ad80796
alex3347 <821882886@qq.com>
2017-11-28 21:07:19 +0800

    discover page completed

--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -25,12 +25,40 @@ export default class Discover extends Component {
                         </div>
                         <div className={styles.detail}>
                             <div className={styles.title}>巴拉巴拉拉</div>
-                            <div className={styles.time}>巴拉巴拉拉</div>
-                            <div className={styles.describe}>巴拉巴拉拉</div>
+                            <div className={styles.time}>刚刚发表</div>
+                            <div className={styles.describe}><i>#首届厨艺大赛#</i>自己做的芒果蛋糕、披萨和榴莲月饼，做的不好请大家多多包涵，希望各位朋友多多点赞，随便帮我点一下上次发的参赛作品，谢谢大家了！</div>
                             <div className={styles.product}>
                                 <img src={require('./images/product1.png')} alt=""/>
                                 <img src={require('./images/product2.png')} alt=""/>
                                 <img src={require('./images/product3.png')} alt=""/>
+                            </div>
+                            <div className={styles.bottom}>
+                                <div className={styles.left}>来自 OPPO x6</div>
+                                <div className={styles.right}>
+                                    <div className={styles.comment}>
+                                        <i className='iconfont icon-pinglun'/>
+                                        <s>3</s>
+                                    </div>
+                                    <div className={styles.good}>
+                                        <i className='iconfont icon-dianzan'/>
+                                        <s>16</s>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.item}>
+                        <div className={styles.imgContainer}>
+                            <img src={require('./images/avatar2.png')} alt=""/>
+                        </div>
+                        <div className={styles.detail}>
+                            <div className={styles.title}>樱桃小丸子</div>
+                            <div className={styles.time}>刚刚发表</div>
+                            <div className={styles.describe}><i>#下午茶#</i>受台风影响，今儿个算是凉快了。午睡过后，来点下午茶，准备一些自己亲手做的小点心，外加一杯卡布奇诺，吹吹小风，这日子真惬意....</div>
+                            <div className={styles.product}>
+                                <img src={require('./images/product4.png')} alt=""/>
+                                <img src={require('./images/product5.png')} alt=""/>
+                                <img src={require('./images/product6.png')} alt=""/>
                             </div>
                             <div className={styles.bottom}>
                                 <div className={styles.left}>来自 OPPO x6</div>
